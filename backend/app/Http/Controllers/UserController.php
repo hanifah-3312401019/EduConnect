78578efb8f9409e97d648fe52292852650e3fab6
melanieputri03 <melanieputri2403@gmail.com>
2025-10-06 10:52:57 +0700

    melani commit

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return response()->json(User::all());
+    }
+
+    public function show($id)
+    {
+        $user = User::find($id);
+        if (!$user) {
+            return response()->json(['message' => 'User tidak ditemukan'], 404);
+        }
+        return response()->json($user);
+    }
+}
