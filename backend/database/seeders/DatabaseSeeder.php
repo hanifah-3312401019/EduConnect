9716495b17536ddc9ce49c86297792c3b91b80a1
Hanifah Dwi Cahayarani_3312401019 <hanifahdwicahayarani@gmail.com>
2025-12-30 12:14:38 +0700

    Refactor seeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            AdminSeeder::class,
             GuruSeeder::class,
             OrangTuaSeeder::class,
             KelasSeeder::class,
